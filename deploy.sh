03fba8132c978f3b5810bb200e241400a0c43f3c
scott@ubuntu <scozv@yandex.com>
2016-09-04 03:32:30 +0800

    update the comment of deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,35 +9,35 @@ BOLERO_DPKG_FULLNAME="bolero-server_1.0-SNAPSHOT"
 
 BOLERO_ENV='Prod'
 BOLERO_HTTP_PORT="9000"
-echo '准备发布bolero-server，默认为生产发布'
-echo '即，appkey和secret都使用真实编号'
-echo '请问是发布为测试环境还是生产环境 ?'
-echo '测试发布请键入Test，否则默认为生产：'
+echo 'Ready for deployment, Release Env as default'
+echo '  appkey and secret are all in Release Env'
+echo 'Confirm the Env Release or Test ?'
+echo '  Type Test, otherwise will be in Release Mode:'
 read BOLERO_ENV
 
-echo '修改配置文件'
+echo 'Switch to Release Conf'
 change_conf() {
   # 修复之前的任何错误更改
   git checkout build.sbt
 
   if [ 'Test' == "$BOLERO_ENV" ]; then
-    echo '测试环境需要修改配置文件'
+    echo 'Test Conf'
     BOLERO_DPKG_SERVERNAME="bolero-server-$BOLERO_GIT_BRANCH"
-    echo "将Git分支添加到Server Name: $BOLERO_DPKG_SERVERNAME"
+    echo "Append git branch to service name: $BOLERO_DPKG_SERVERNAME"
     sed -i "s/bolero-server/$BOLERO_DPKG_SERVERNAME/" build.sbt
 
     BOLERO_DPKG_VERSION="1.0-SNAPSHOT-$BOLERO_GIT_SHA1"
-    echo "将Git sha1添加到文件名：$BOLERO_DPKG_VERSION"
+    echo "Append git sha1 to service name: $BOLERO_DPKG_VERSION"
     sed -i "s/1.0-SNAPSHOT/$BOLERO_DPKG_VERSION/" build.sbt
 
     BOLERO_DPKG_FULLNAME="${BOLERO_DPKG_SERVERNAME}_$BOLERO_DPKG_VERSION"
-    echo "完整的文件名为：$BOLERO_DPKG_FULLNAME"
+    echo "Final service name is: $BOLERO_DPKG_FULLNAME"
 
     conf_str_prod='"\-Dconfig.file=\/usr\/share\/\${packageName.value}\/conf\/release.conf"'
     conf_str_eval='"\-Dconfig.file=\/usr\/share\/\${packageName.value}\/conf\/application.conf"'
     sed -i "s/$conf_str_prod/$conf_str_eval/g" build.sbt
 
-    echo '请输入四位测试发布的HTTP Port，默认为9000：'
+    echo 'Type the Test Mode HTTP Port, 9000 as default: '
     read BOLERO_HTTP_PORT
 
     if [[ "$BOLERO_HTTP_PORT" =~ [0-9][0-9][0-9][0-9] ]]; then
@@ -45,8 +45,8 @@ change_conf() {
     else
       BOLERO_HTTP_PORT="9000"
     fi
-    echo "使用配置：-Dhttp.port=$BOLERO_HTTP_PORT"
-    sed -i "s/-Dhttp.port=9000/-Dhttp.port=$BOLERO_HTTP_PORT/g" build.sbt
+    echo "Apply the conf: -Dhttp.port=$BOLERO_HTTP_PORT"
+    sed -i "s/-Dhttp.port=9000git s/-Dhttp.port=$BOLERO_HTTP_PORT/g" build.sbt
 
   else
     BOLERO_DPKG_FULLNAME="bolero-server_1.0-SNAPSHOT"
@@ -54,7 +54,7 @@ change_conf() {
 
   # activator test
   activator playUpdateSecret
-  echo "完整的配置清单"
+  echo "Review the conf"
   cat build.sbt
   sbt debian:packageBin
   # activator run
@@ -64,7 +64,7 @@ change_conf() {
   git checkout conf/application.conf
 }
 
-echo '生成Native Package'
+echo 'Compile to Native Package'
 change_conf
 
 # http://askubuntu.com/a/615086
@@ -84,11 +84,11 @@ change_conf
 
 # Ubuntu 14.04 run only once
 # sudo apt-get -y install fakeroot
-echo '安装并启动'
-echo "当前为 $BOLERO_ENV 环境"
-echo "安装包 ${BOLERO_DPKG_FULLNAME}_all"
+echo 'Install the deb and start service'
+echo "Currently, we are in $BOLERO_ENV Mode"
+echo "Package name: ${BOLERO_DPKG_FULLNAME}_all"
 BOLERO_INST_SH="sudo dpkg -i target/${BOLERO_DPKG_FULLNAME}_all.deb"
-echo "请执行 $BOLERO_INST_SH"
+echo "Please execute $BOLERO_INST_SH"
 
 # stop | start
 # sudo start bolero-server
