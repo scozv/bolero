46c0ee1919f1a9bfbe5c35eb044d3d7abd6d289a
scott@ubuntu <scozv@users.noreply.github.com>
2016-09-03 09:31:08 +0800

    see #3 model of Transaction

---
+++ app/models/Transaction.scala
@@ -0,0 +1,36 @@
+package models
+
+import models.interop.{CanBeHierarchicInstance, CanBeHierarchicObject, CanBeJsonfied}
+
+case class Transaction(_id: String, amount: Double, tp: String, rootId: String)
+  extends CanBeHierarchicInstance {
+
+  val isRoot = rootId.isEmpty
+}
+
+object Transaction extends CanBeJsonfied[Transaction] with CanBeHierarchicObject {
+  override val rootFieldName = "parent_id"
+
+  import play.api.libs.json._
+  import play.api.libs.functional.syntax._
+
+  implicit val writes = new OWrites[Transaction] {
+    def writes(tx: Transaction) = Json.obj(
+      "_id" -> tx._id,
+      "amount" -> tx.amount,
+      "type" -> tx.tp,
+      "parent_id" -> tx.rootId
+    )
+  }
+
+  implicit val reads: Reads[Transaction] = (
+    (__ \ "_id").read[String] and
+      (__ \ "amount").read[Double] and
+      (__ \ "type").read[String] and
+      (__ \ rootFieldName).read[String]
+    ) (Transaction.apply _)
+}
+
+
+
+
